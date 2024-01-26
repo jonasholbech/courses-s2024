fb055f554f6e451572de7d2320ac634fc87ed50a
Jonas Holbech <jh@jonasholbech.dk>
2024-01-26 12:50:00 +0100

    t7/1

---
+++ public/snippets/t7/1.introduction/louise.js
@@ -0,0 +1,8 @@
+let louise = {
+  name: "Louise Holbek",
+  education: "Kand it'er - digital design & kommunikation",
+  jobs: ["Mor til en teenager", "Lektor", "UX designer"],
+  previousJobs: ["konsulent", "UX specialist", "webredaktør", "selvstændig"],
+  hasBeenToRoskildeSince: 1979,
+  siblings: 4,
+};
