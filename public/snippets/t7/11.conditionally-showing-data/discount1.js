142658caae7b8b71d162bc755dc610849834e0af
Jonas Holbech <jh@jonasholbech.dk>
2024-02-05 19:24:08 +0100

    t7/11 conditional data

--- public/snippets/t7/11.conditionally-showing-data/discount1.js
+++ public/snippets/t7/11.conditionally-showing-data/discount1.js
@@ -1,3 +1,5 @@
 if (product.discount) {
   clone.querySelector(".product").classList.add("discount");
+  //or
+  //clone.querySelector(".product").classList.remove("discount");
 }
---
+++ public/snippets/t7/11.conditionally-showing-data/discount3.js
@@ -0,0 +1,4 @@
+if (!product.discount) {
+  //!product.discount means NOT product.discount
+  clone.querySelector(".discount").remove();
+}
